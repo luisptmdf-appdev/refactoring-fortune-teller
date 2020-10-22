c44262eff42d70d19ea992f2c281b749a28e5580
luisptmdf <luis@chicagobooth.edu>
2020-10-22 06:13:50 +0000

    signs ready / before deleting code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
 
+  def sign 
+
+    @sign = params.fetch("sign")
+
+    sign_info = Zodiac.list.fetch(@sign.to_sym)
+
+    @horoscope = sign_info.fetch(:horoscope)
+
+    @array_of_numbers = Array.new
+    5.times do
+      another_number = rand(1...100)
+      @array_of_numbers.push(another_number)
+    end
+
+    render({:template => "zodiac_templates/sign.html.erb"})
+
+  end
+
 end
